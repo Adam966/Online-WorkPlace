85bce0bc849ae06287d02e09bd0cb482c89ee76e
Adam966 <adam.ivan@akademiasovy.sk>
2021-01-01 01:05:29 +0100

    add avatar img

--- OnlineWorkPlace/src/app/shared/user-label/user-label.component.ts
+++ OnlineWorkPlace/src/app/shared/user-label/user-label.component.ts
@@ -10,9 +10,20 @@ export class UserLabelComponent implements OnInit {
   @Input()
   user: User;
 
+  showImage = true;
+  initials: string;
+  photo: string;
+
   constructor() {}
 
   ngOnInit(): void {
+    if (this.user.photo !== 0) {
+      this.showImage = true;
+      this.photo = 'http://localhost:3000/user-photo?userId=' + this.user.photo.toString();
+    } else {
+      this.showImage = false;
+      this.initials = this.user.username.charAt(0) + this.user.usersurname.charAt(0);
+      this.photo = '';
+    }
   }
-
 }
