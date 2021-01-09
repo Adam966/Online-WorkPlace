d08f9b12b6da65da2e0339f3bea3e1affd2675a0
Adam966 <adam.ivan@akademiasovy.sk>
2021-01-09 23:06:31 +0100

    several improvements

--- OnlineWorkPlace/src/app/components/container/workplace-screen/workplace-element/workplace-element.component.ts
+++ OnlineWorkPlace/src/app/components/container/workplace-screen/workplace-element/workplace-element.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {WorkplaceElementModel} from '../../../../models/workplacemodels/workplaceelement.model';
 import {ChecklistModel} from '../../../../models/workplacemodels/checklist.model';
+import {LabelModel} from '../../../../models/label.model';
+import {User} from '../../../../models/application-models/user.model';
 
 @Component({
   selector: 'app-workplace-element',
@@ -15,9 +17,15 @@ export class WorkplaceElementComponent implements OnInit {
   taskInfo: string;
   isCompleted: boolean;
 
+  assignedLabels: LabelModel[];
+  assignedUsers: User[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.assignedUsers = this.element.assignedUsers ?? [];
+    this.assignedLabels = this.element.assignedLabels ?? [];
+
     if (this.element.hasOwnProperty('tasks')) {
       this.task = true;
       this.checkTask(this.element as ChecklistModel);
