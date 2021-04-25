f66af71659ce0623e6caacddf2e54f6d1ef1450b
Adam Ivan <adam.ivan@kosickaakademia.sk>
2021-04-25 22:25:24 +0200

    add pagination to chat, fix functionality

--- OnlineWorkPlace/src/app/components/container/thread-chat/thread-chat.component.ts
+++ OnlineWorkPlace/src/app/components/container/thread-chat/thread-chat.component.ts
@@ -1,21 +1,33 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {ChatService} from '../../../services/chat-socket-api/chat.service';
 import {UserModel} from '../../../models/application-models/user.model';
 import {Select} from '@ngxs/store';
 import {LoginState} from '../../../store/login';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, Observable, of} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {threadId} from 'worker_threads';
 import {MessageModel} from '../../../models/message.model';
 import {filter} from 'rxjs/operators';
 import {FormControl} from '@angular/forms';
+import {UtilsMessage} from '../../../shared/utils/utils-message';
 
 @Component({
   selector: 'app-thread-chat',
   templateUrl: './thread-chat.component.html',
   styleUrls: ['./thread-chat.component.css']
 })
-export class ThreadChatComponent implements OnInit, OnDestroy {
+export class ThreadChatComponent implements OnInit, OnDestroy, AfterViewChecked, AfterViewInit {
+  @ViewChild('container')
+  container: ElementRef;
+
   @Select(LoginState)
   user$!: Observable<UserModel>;
 
@@ -26,14 +38,30 @@ export class ThreadChatComponent implements OnInit, OnDestroy {
 
   private user: UserModel;
   private readonly threadId: string;
-  private page = 1;
+  private page = 0;
   notify = '';
+  private notOldMessagesAnymore = false;
+  loadingMessages = false;
 
   constructor(private chatService: ChatService, private route: ActivatedRoute) {
     this.threadId = this.route.snapshot.paramMap.get('threadId');
     this.user$.subscribe(user => this.user = user);
+    this.getOldMessages();
+  }
+
+  private getOldMessages(): void {
+    this.loadingMessages = true;
     this.chatService.getOldMessages(this.threadId, this.page.toString())
-      .subscribe(messages => this.messages.push(...messages));
+      .subscribe(messages => {
+        this.messages.push(...messages);
+        this.page = this.page + 1;
+        this.loadingMessages = false;
+      }, (err => {
+        this.notOldMessagesAnymore = true;
+        this.loadingMessages = false;
+        UtilsMessage.showMessage(err.error.message, UtilsMessage.MESSAGE_STATUS_ERROR);
+        return of(err);
+      }));
   }
 
   ngOnInit(): void {
@@ -42,14 +70,14 @@ export class ThreadChatComponent implements OnInit, OnDestroy {
         this.connectionStatus.next(true);
       });
 
-    this.messageInput.valueChanges
+/*    this.messageInput.valueChanges
       .subscribe(text => {
         if ((text as string).length > 5) {
           this.chatService.notifyTyping(this.threadId, true);
         } else {
           this.chatService.notifyTyping(this.threadId, false);
         }
-      });
+      });*/
 
     this.connectionStatus
       .pipe(
@@ -61,14 +89,17 @@ export class ThreadChatComponent implements OnInit, OnDestroy {
   }
 
   sendMessage(message: string): void {
-    // TODO set date to be utc
-    const threadMessage = {
-      description: message,
-      timestamp: new Date().toLocaleString(),
-      senderUser: this.user
-    };
-    this.chatService.sendMessage(threadMessage, this.threadId);
-    this.chatService.notifyTyping(this.threadId, false);
+    if (message) {
+      const threadMessage = {
+        description: message,
+        timestamp: new Date().toLocaleString(),
+        senderUser: this.user
+      };
+      this.chatService.sendMessage(threadMessage, this.threadId);
+      this.messageInput.patchValue('');
+      this.scrollToBottom();
+      // this.chatService.notifyTyping(this.threadId, false);
+    }
   }
 
   ngOnDestroy(): void {
@@ -93,5 +124,24 @@ export class ThreadChatComponent implements OnInit, OnDestroy {
           }
         }
       );
+  }
+
+  ngAfterViewChecked(): void {
+  }
+
+  onTop($event: Event): void {
+    if ($event.target.scrollTop === 0 && !this.notOldMessagesAnymore) {
+      this.getOldMessages();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom(): void {
+    setTimeout(() => {
+      this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
+    }, 10);
   }
 }
